cb041eb14edfa8d37f8a8d1db34271e3dcde07af
Jean Thibaut <thibaut-jean@live.fr>
2021-07-13 16:50:54 +0200

    add authenticate fully

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Component\Security\Core\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
@@ -20,12 +19,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 #[Route('/project')]
 class ProjectController extends AbstractController
 {
-    // private $security;
- 
-    // public function __construct(Security $security)
-    // {
-    //     $this->security = $security;
-    // }
 
     #[Route('/', name: 'project_index', methods: ['GET'])]
     public function index(ProjectRepository $projectRepository): Response
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,6 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
 
 #[Route('/task')]
 class TaskController extends AbstractController
